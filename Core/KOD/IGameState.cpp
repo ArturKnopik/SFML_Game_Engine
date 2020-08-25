8f0c3e96f0185b7ab3ad3c2701d129c10666dbd0
Artur <artur.knopik.1991@gmail.com>
2020-08-25 21:11:59 +0200

    fixes

--- Core/KOD/IGameState.cpp
+++ Core/KOD/IGameState.cpp
@@ -10,14 +10,6 @@ KOD::IGameState::IGameState()
 
 void KOD::IGameState::draw()
 {
-  sf::RectangleShape rect;
-  rect.setFillColor(sf::Color::Red);
-  rect.setSize({ 32, 32 });
-  rect.setPosition({ 499, 600 });
-  m_game.lock()->m_window->draw(rect);
-
-
-
   sf::View view;
   view.setSize(m_game.lock()->m_window->getSize().x, m_game.lock()->m_window->getSize().y);
   view.setCenter(m_camera.getPosition());
